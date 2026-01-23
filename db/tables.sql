3c3ff93ce276aa0092350a9acad64ddde2448bfc
FarmBera <101335222+FarmBera@users.noreply.github.com>
2026-01-23 22:22:42 +0900

    Alter Table

--- db/tables.sql
+++ db/tables.sql
@@ -3,11 +3,14 @@ USE webserver;
 CREATE TABLE IF NOT EXISTS logs
 (
     id         BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    ip_addr    VARCHAR(100) DEFAULT '0.0.0.0',
-    content    VARCHAR(255) DEFAULT '-',
-    created_at DATETIME     DEFAULT CURRENT_TIMESTAMP,
+    ip_addr    VARCHAR(100) NOT NULL DEFAULT '0.0.0.0',
+    code       INT          NOT NULL DEFAULT 0,
+    msg        VARCHAR(255),
+    content    VARCHAR(1024),
+    created_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     -- index
     INDEX idx_ip (ip_addr),
+    INDEX idx_code (code),
     INDEX idx_content (content),
     INDEX idx_created_at (created_at)
 ) DEFAULT CHARACTER SET = 'utf8mb4'
